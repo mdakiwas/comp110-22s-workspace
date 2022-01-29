833bc9a7b324f705f7f21e1eadebd3f099fdce99
Marlette Jessa Dakiwas <mdakiwas@unc.edu>
2022-01-29 01:40:08 -0500

    Progress on Exercise 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -6,10 +6,10 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 secret: str = "python"
-guess = input(f"What is your {len(secret)}-letter guess? ")  # asking user input for a word guess
+guess: str = input(f"What is your {len(secret)}-letter guess? ")  # asking user input for a word guess
 
 while len(guess) != len(secret): 
-    guess: str = input(f"That was not {len(secret)} letters! Try again: ")  # re-asking user input if length is wrong
+    guess = input(f"That was not {len(secret)} letters! Try again: ")  # re-asking user input if length is wrong
 
 i: int = 0
 result: str = "" 
